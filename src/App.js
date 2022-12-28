e28f92b6f7a354f5958d06a7dd6236ce1a5d3072
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2022-12-28 22:46:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Screen from './Components/Screen';
+function App() {
+  return (
+    <div className="App">
+      <Screen/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Screen.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown'
+
+const Screen = () => {
+    const [input,setInput] = useState('');
+    const output=(e)=>{
+        setInput(e.target.value)
+    }
+  return (
+    <div className='main'>
+      <div>
+        <textarea cols={57} rows={30} onChange={output} value={input}></textarea>
+      </div>
+  
+        <div><ReactMarkdown>{input}</ReactMarkdown></div>
+     
+    </div>
+  )
+}
+
+export default Screen;
